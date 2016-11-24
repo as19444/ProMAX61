74e257e3a91cd70d8b37e7ffc89f82ce1c7d5dc7
Ashok <ashok@abs.ae>
2016-11-24 10:41:40 +0400

    Initial Commit

---
+++ ProMAX61/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProMAX61.MvcApplication" Language="C#" %>
---
+++ ProMAX61/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProMAX61.Startup))]
+namespace ProMAX61
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
